de7b023753df60375b05ac1026b2919064fa7bfa
tamy_le <tamy.le.cs@gmail.com>
2024-03-31 11:04:48 +0900

    feat(add history): able to show moves

--- tic-tac-toe/src/components/Game.js
+++ tic-tac-toe/src/components/Game.js
@@ -6,25 +6,21 @@ const Game = () => {
   const minGridSize = 3;
   const maxGridSize = 10;
   const [inputGridSize, setInputGridSize] = useState(minGridSize);
-  const [currentBoard, setCurrentBoard] = useState(
-    Array(minGridSize ** 2).fill("")
-  );
+  const [history, setHistory] = useState([Array(minGridSize ** 2).fill("")]);
   const [gridSize, setGridSize] = useState(minGridSize);
   const [currentMove, setCurrentMove] = useState(0);
   const [status, setStatus] = useState("Game Start");
   const [winner, setWinner] = useState();
 
   const handleClickedSquare = (index) => {
-    console.log("aaa");
-
-    if (currentBoard[index] || winner) {
+    if (history[currentMove][index] || winner) {
       return;
     }
-    const newBoard = [...currentBoard];
+    const newBoard = [...history[currentMove]];
     newBoard[index] = currentMove % 2 == 0 ? "X" : "O";
-    setCurrentBoard(newBoard);
+    setHistory([...history, newBoard]);
     setCurrentMove(currentMove + 1);
-    setStatus(`Turn of ${newBoard[index] == "O" ? "X" : "O"}`);
+    setStatus(`Next player: ${newBoard[index] == "O" ? "X" : "O"}`);
     if (checkWinner(newBoard, index, gridSize)) {
       setWinner(newBoard[index]);
       setStatus(`Winner is ${newBoard[index]}`);
@@ -38,16 +34,21 @@ const Game = () => {
     }
     if (newSize >= minGridSize && newSize <= maxGridSize) {
       setGridSize(newSize);
-      setCurrentBoard(Array(newSize ** 2).fill(""));
+      setHistory(Array(newSize ** 2).fill(""));
       setStatus("Game Start");
       setWinner("");
       setCurrentMove(0);
     }
     setInputGridSize(newSize);
   };
+
+  const moves = history.map((squares, move) => {
+    let description = move > 0 ? `Go to move #${move}` : "Go to game start";
+    return <li>{description}</li>;
+  });
   return (
-    <main className="ml-5">
-      <div className="block pb-5">
+    <main className="ml-5 flex">
+      <div className="block pb-5 text-white">
         <input
           className="text-black"
           name="grid-size"
@@ -57,15 +58,17 @@ const Game = () => {
           max={10}
           onChange={handleGridSize}
         />
-      </div>
-      <div className="text-white">
+
         <h1>{status}</h1>
+        <div className="flex">
+          <Board
+            gridSize={gridSize}
+            currentBoard={history[currentMove]}
+            handleClickedSquare={handleClickedSquare}
+          />
+          <ol className="ml-10">{moves}</ol>
+        </div>
       </div>
-      <Board
-        gridSize={gridSize}
-        currentBoard={currentBoard}
-        handleClickedSquare={handleClickedSquare}
-      />
     </main>
   );
 };
