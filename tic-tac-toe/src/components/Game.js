f60cba27a212f2b0c5e8a465887f23a1b548c006
tamy_le <tamy.le.cs@gmail.com>
2024-03-31 22:34:26 +0900

    refactor(move winnercheck to utils): typo reverse and move script

--- tic-tac-toe/src/components/Game.js
+++ tic-tac-toe/src/components/Game.js
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import Board from "./Board";
-import { checkWinner, checkDraw, convertIndexToRowCol } from "./WinnerCheck";
+import {
+  checkWinner,
+  checkDraw,
+  convertIndexToRowCol,
+} from "../utils/WinnerCheck";
 
 const Game = () => {
   const minGridSize = 3;
@@ -116,7 +120,7 @@ const Game = () => {
               className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
               onClick={() => setToggle(!toggle)}
             >
-              Reserve Moves Order
+              Reverse Moves Order
             </button>
             <ol>{toggle ? [...moves].reverse() : moves}</ol>
           </div>
