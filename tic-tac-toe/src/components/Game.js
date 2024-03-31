a0b27a9c16260ad3073fb959544586ddaa664685
tamy_le <tamy.le.cs@gmail.com>
2024-03-31 12:00:56 +0900

    feat(add toggle): to reserve order of moves

--- tic-tac-toe/src/components/Game.js
+++ tic-tac-toe/src/components/Game.js
@@ -10,7 +10,8 @@ const Game = () => {
   const [gridSize, setGridSize] = useState(minGridSize);
   const [currentMove, setCurrentMove] = useState(0);
   const [status, setStatus] = useState("Game Start");
-  const [winner, setWinner] = useState();
+  const [winner, setWinner] = useState("");
+  const [toggle, setToggle] = useState(false);
 
   const handleClickedSquare = (index) => {
     if (
@@ -51,7 +52,6 @@ const Game = () => {
   };
 
   const moves = history.map((squares, move) => {
-    console.log(move);
     let description = move > 0 ? `Go to move #${move}` : "Go to game start";
     return (
       <li key={move}>
@@ -88,7 +88,16 @@ const Game = () => {
             currentBoard={history[currentMove]}
             handleClickedSquare={handleClickedSquare}
           />
-          <ol className="ml-10">{moves}</ol>
+          <div className="ml-10">
+            <button
+              type="button"
+              class="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              onClick={() => setToggle(!toggle)}
+            >
+              Reserve Moves Order
+            </button>
+            <ol>{toggle ? [...moves].reverse() : moves}</ol>
+          </div>
         </div>
       </div>
     </main>
