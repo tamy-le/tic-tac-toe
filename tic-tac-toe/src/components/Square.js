6da3c2160190a8b5e52048d8838e3dc663e6fe0a
tamy_le <tamy.le.cs@gmail.com>
2024-04-01 23:35:56 +0900

    refactor(css and format): use custom css and reformat by prettier

--- tic-tac-toe/src/components/Square.js
+++ tic-tac-toe/src/components/Square.js
@@ -1,13 +1,13 @@
 const Square = ({ value, isWonSquare, handleClicked }) => {
-  const backgroundColor = isWonSquare ? "bg-rose-300" : "bg-white";
-  const textColor = isWonSquare ? "text-red-600	" : "text-black";
-  const fontWeight = isWonSquare ? "font-semibold" : "font-light";
-  const className = `border-4 border-indigo-500/100 float-left size-16 box-border ${fontWeight} ${textColor} text-center ${backgroundColor}`;
+	const backgroundColor = isWonSquare ? "bg-rose-300" : "bg-white";
+	const textColor = isWonSquare ? "text-red-600	" : "text-black";
+	const fontWeight = isWonSquare ? "font-semibold" : "font-light";
+	const className = `square ${backgroundColor} ${fontWeight} ${textColor}`;
 
-  return (
-    <button className={className} onClick={handleClicked}>
-      {value}
-    </button>
-  );
+	return (
+		<button className={className} onClick={handleClicked}>
+			{value}
+		</button>
+	);
 };
 export default Square;
