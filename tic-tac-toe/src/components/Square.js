1e8542208b6374c05e08332ee2af867a1c2e7bed
tamy_le <tamy.le.cs@gmail.com>
2024-04-03 01:50:57 +0900

    style(trailing space): remove

--- tic-tac-toe/src/components/Square.js
+++ tic-tac-toe/src/components/Square.js
@@ -1,6 +1,6 @@
 const Square = ({ value, isWonSquare, handleClicked }) => {
 	const backgroundColor = isWonSquare ? "bg-rose-300" : "bg-white";
-	const textColor = isWonSquare ? "text-red-600	" : "text-black";
+	const textColor = isWonSquare ? "text-red-600" : "text-black";
 	const fontWeight = isWonSquare ? "font-semibold" : "font-light";
 	const className = `square ${backgroundColor} ${fontWeight} ${textColor}`;
 
